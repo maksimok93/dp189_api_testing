3b89ff10b099b2ab4f16098d189abbb96235f807
Danya1998 <yurchenkodanil1998@gmail.com>
2020-08-04 10:14:47 +0300

    refatorig test_auth_controller.py

--- tests/test_auth_controller.py
+++ tests/test_auth_controller.py
@@ -1,1 +1,82 @@
+import requests
+import pytest
 
+import requests
+
+
+class TestAuthController:
+    """Methods for testing users with different access rights."""
+
+    def setup(self) -> None:
+        """Setup for the tests.
+        :return: None
+        """
+        self.auth_url = 'https://softserve2020romeo.herokuapp.com/api/auth'
+
+    def get_token(self, credentials):
+        authorization = requests.post(url=self.auth_url, json=credentials)
+        return authorization.headers['Authorization']
+
+
+class TestAdminAuth(TestAuthController):
+    admin_credentials_json = {"email": "adminsoft2020@gmail.com", "password": "qwerty"}
+
+    def test_admin_login(self) -> None:
+        """Testing administrator login.
+        :return: None
+        """
+
+        admin_login = requests.post(url=self.auth_url, json=self.admin_credentials_json)
+
+        assert admin_login.status_code == 200
+
+    def test_admin_logout(self):
+        token = self.get_token(self.admin_credentials_json)
+        admin_logout = requests.delete(url=self.auth_url, headers={'Authorization': f'{token}'})
+        assert admin_logout.status_code == 200
+
+
+class TestMentorAuth(TestAuthController):
+    mentor_credentials_json = {"email": "martyn@nmu.one", "password": "XSRLvaIt"}
+
+    def test_mentor_login(self) -> None:
+        """Testing mentor login.
+        :return: None
+        """
+        mentor_login = requests.post(url=self.auth_url, json=self.mentor_credentials_json)
+
+        assert mentor_login.status_code == 200
+
+    def test_mentor_logout(self):
+        token = self.get_token(self.mentor_credentials_json)
+        mentor_logout = requests.delete(url=self.auth_url, headers={'Authorization': f'{token}'})
+        assert mentor_logout.status_code == 200
+
+
+class TestStudentAuth(TestAuthController):
+    student_credentials_json = {"email": "kuprienko.v@nmu.one", "password": "OvByYxjN"}
+
+    def test_student_login(self) -> None:
+        """Testing student login.
+        :return: None
+        """
+        student_login = requests.post(url=self.auth_url, json=self.student_credentials_json)
+
+        assert student_login.status_code == 200
+
+    def test_student_logout(self) -> None:
+        token = self.get_token(self.student_credentials_json)
+        student_logout = requests.delete(url=self.auth_url, headers={'Authorization': f'{token}'})
+        assert student_logout.status_code == 200
+
+
+class TestInvalidAuth(TestAuthController):
+    invalid_credentials_json = {"email": "invalid", "password": "invalid"}
+
+    def test_login_invalid_data(self) -> None:
+        """Testing login without access rights.
+        :return: None
+        """
+        invalid_login = requests.post(url=self.auth_url, json=self.invalid_credentials_json)
+
+        assert invalid_login.status_code == 403
