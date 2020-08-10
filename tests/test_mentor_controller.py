686c48d8218af95e1a2ae72cef75e9deae1eae4f
Maksym Bezruchko <maksimok.dnepr@gmail.com>
2020-08-10 13:24:25 +0300

    Automate-mentor-controller Updated test for updateMentor (PUT).

--- tests/test_mentor_controller.py
+++ tests/test_mentor_controller.py
@@ -1,14 +1,22 @@
 import pytest
 from routes import *
-from tests.config import get_test_data, get_test_data_json
+from tests.config import get_test_data
 from models import get_response
 
 
 class TestMentorController:
+    """Tests for Mentor Controller for testing users with different access rights."""
 
     @pytest.mark.parametrize('email,password,expected_status_code',
                              get_test_data('test_mentor_controller_put.csv'))
-    def test_update_mentor_PUT(self, email, password, expected_status_code):
+    def test_update_mentor_PUT(self, email, password, expected_status_code) -> None:
+        """Parameterized test that checks response to PUT request to update mentor
+        details for a specific course for users with different access types.
+        :param user email: string
+        :param user password: string
+        :param expected_status_code: string
+        :return: None
+        """
         credentials = {"email": email, "password": password}
-        response = get_response('put', get_mentor_url(''), credentials, "test_course_controller_put.json")
+        response = get_response('put', get_mentor_url('32'), credentials, "test_mentor_controller_put.json")
         assert response.status_code == int(expected_status_code)
