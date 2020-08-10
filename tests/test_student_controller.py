bc4b0ef812afe8d46ab117658ae0dc660da89b0f
Danya1998 <yurchenkodanil1998@gmail.com>
2020-08-10 20:32:07 +0300

    add test_student_controller.py automation tests

---
+++ tests/test_student_controller.py
@@ -0,0 +1,54 @@
+import pytest
+from routes import STUDENT, get_student_url
+from tests.config import get_test_data, get_test_data_json
+from models import get_response
+from tests.base_test import BaseTest
+
+
+class TestStudentController(BaseTest):
+    @pytest.mark.parametrize('email,password,expected_status_code,expected_body',
+                             get_test_data('test_student_controller_get.csv'))
+    def test_get_all_students(self, email: str, password: str, expected_status_code: str, expected_body: str):
+        """Parameterized test that checks response to GET request to get all students
+                for users with different access types.
+
+        :param user email: string
+        :param user password: string
+        :param expected_status_code: string
+        :param expected_body: string (JSON file name with test data)
+        :return: None
+        """
+        credentials = {"email": email, "password": password}
+        response = get_response('get', STUDENT, credentials)
+        assert response.status_code == int(expected_status_code)
+
+        if response.status_code == 200:
+            assert response.json() == get_test_data_json(f'expectedJson/{expected_body}')
+
+    @pytest.mark.parametrize('email,password,expected_status_code',
+                             get_test_data('test_student_controller_post.csv'))
+    def test_create_student(self, email, password, expected_status_code):
+        credentials = {"email": email, "password": password}
+        response = get_response('post', STUDENT, credentials, 'test_student_controller_post.json')
+        assert response.status_code == int(expected_status_code)
+
+    @pytest.mark.parametrize('email,password,expected_status_code',
+                             get_test_data('test_student_controller_get_by_id.csv'))
+    def test_get_student_by_id(self, email, password, expected_status_code):
+        credentials = {"email": email, "password": password}
+        response = get_response('get', get_student_url('8'), credentials)
+        assert response.status_code == int(expected_status_code)
+
+    @pytest.mark.parametrize('email,password,expected_status_code',
+                             get_test_data('test_student_controller_put.csv'))
+    def test_update_student(self, email, password, expected_status_code):
+        credentials = {"email": email, "password": password}
+        response = get_response('put', get_student_url('8'), credentials, "test_student_controller_put.json")
+        assert response.status_code == int(expected_status_code)
+
+    @pytest.mark.parametrize('email,password,expected_status_code',
+                             get_test_data('test_student_controller_delete.csv'))
+    def test_delete_student(self, email, password, expected_status_code):
+        credentials = {"email": email, "password": password}
+        response = get_response('delete', get_student_url('57'), credentials)
+        assert response.status_code == int(expected_status_code)
