c906a67b27322fa5e8f86caec17f7e80b1243698
Tanya-Kr <tanya.krug7@gmail.com>
2020-08-11 12:18:21 +0300

    Add tests to Lesson Controller GET

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,24 +1,11 @@
-import time
-
 from tests.database_controller import drop_all_tables, execute_sql_file
 
 
 class BaseTest:
-    # def setup(self):
-    #     drop_all_tables()
-    #     execute_sql_file('soft.sql')
-    #
-    # def teardown(self):
-    #     drop_all_tables()
-    #     execute_sql_file('soft.sql')
-
-    @classmethod
-    def setup_class(cls):
+    def setup(self):
         drop_all_tables()
         execute_sql_file('soft.sql')
-        time.sleep(10)
 
-    @classmethod
-    def teardown_class(cls):
+    def teardown(self):
         drop_all_tables()
         execute_sql_file('soft.sql')
